29830e1b80ffcdd5529254b8db45b0a2baf12d3d
aptem334 <aptem334@gmail.com>
2021-04-27 22:21:47 +0300

    многомодульный проект мавен

---
+++ myFirstProject/CalcOutput/src/main/java/Printer.java
@@ -0,0 +1,8 @@
+import neoflex.ru.example.calc.Calculator;
+
+public class Printer {
+    public static void main(String[] args) {
+        int result = Calculator.multiplication(5,7);
+        System.out.println(result);
+    }
+}
---
+++ neoflex/activeRecord/src/main/java/Active.java
@@ -0,0 +1,5 @@
+public class Active {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
