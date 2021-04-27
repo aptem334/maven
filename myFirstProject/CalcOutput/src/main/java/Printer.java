e2c0e27e18761b9b967d2597400292a19c1a09db
aptem334 <aptem334@gmail.com>
2021-04-27 22:30:26 +0300

    многомодульный проект мавен

--- myFirstProject/CalcOutput/src/main/java/Printer.java
+++ myFirstProject/CalcOutput/src/main/java/Printer.java
@@ -1,5 +1,7 @@
 import neoflex.ru.example.calc.Calculator;
 
+import static java.lang.System.*;
+
 public class Printer {
     public static void main(String[] args) {
         int result = Calculator.multiplication(5,7);
